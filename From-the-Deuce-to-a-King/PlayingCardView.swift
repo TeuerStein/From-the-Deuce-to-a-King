9bc648601c2410595786982498681f278b268cb9
TeuerStein <xenkai123456789@gmail.com>
2020-10-05 16:23:29 +0200

    Added a CGRect extensions width funcs offsetBy & zoom and local values leftHalf & rightHalf

--- From-the-Deuce-to-a-King/PlayingCardView.swift
+++ From-the-Deuce-to-a-King/PlayingCardView.swift
@@ -64,3 +64,29 @@ extension PlayingCardView {
         }
     }
 }
+
+extension CGRect {
+    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint {
+        return CGPoint(x: minX + dx, y: minY + dy)
+    }
+}
+
+extension CGRect {
+    func zoom(by zoomFactor: CGFloat) -> CGRect {
+        let zoomedWidth = size.width * zoomFactor
+        let zoomedHeight = size.height * zoomFactor
+        let originX = origin.x + (size.width - zoomedWidth) / 2
+        let originY = origin.y + (size.height - zoomedHeight) / 2
+        return CGRect(origin: CGPoint(x: originX, y: originY), size: CGSize(width: zoomedWidth, height: zoomedHeight))
+    }
+    
+    var leftHalf: CGRect {
+        let width = size.width / 2
+        return CGRect(origin: origin, size: CGSize(width: width, height: size.height))
+    }
+    
+    var rightHalf: CGRect {
+        let width = size.width / 2
+        return CGRect(origin: CGPoint(x: origin.x + width, y: origin.y), size: CGSize(width: width, height: size.height))
+    }
+}
