af361d1a8838db2e1fae58bf9917c73053b96007
TeuerStein <xenkai123456789@gmail.com>
2020-10-13 16:39:46 +0200

    Created a flipCard function and used in the viewDidLoad

--- From-the-Deuce-to-a-King/ViewController.swift
+++ From-the-Deuce-to-a-King/ViewController.swift
@@ -26,6 +26,18 @@ class ViewController: UIViewController {
             let card = cards.remove(at: cards.count.arc4random)
             cardView.rank = card.rank.order
             cardView.suit = card.suit.rawValue
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
+    }
+        
+    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+            }
+        default:
+            break
         }
     }
 }
