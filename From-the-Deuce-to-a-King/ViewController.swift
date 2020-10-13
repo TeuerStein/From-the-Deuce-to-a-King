9475a454c9ab340544ca086a667e1e413ab85e9c
TeuerStein <xenkai123456789@gmail.com>
2020-10-13 16:49:07 +0200

    Added 3D animation for flipCard

--- From-the-Deuce-to-a-King/ViewController.swift
+++ From-the-Deuce-to-a-King/ViewController.swift
@@ -34,7 +34,14 @@ class ViewController: UIViewController {
         switch recognizer.state {
         case .ended:
             if let chosenCardView = recognizer.view as? PlayingCardView {
-                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                UIView.transition(
+                    with: chosenCardView,
+                    duration: 0.6,
+                    options: [.transitionFlipFromLeft],
+                    animations: {
+                        chosenCardView.isFaceUp = !chosenCardView.isFaceUp
+                    }
+                )
             }
         default:
             break
